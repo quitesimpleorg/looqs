58c7a525858c7358749b54fc83e9bdb6fc1c59a6
Albert S <mail@quitesimple.org>
2018-08-09 16:37:30 +0200

    begin groundwork to save pages in docs, not just everything

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
--- Create a table. And an external content fts5 table to index it.
-CREATE TABLE file(id INTEGER PRIMARY KEY, path varchar(4096) UNIQUE, mtime integer, content text);
-CREATE VIRTUAL TABLE file_fts USING fts5(content, content='file', content_rowid='id');
+CREATE TABLE file(id INTEGER PRIMARY KEY, path varchar(4096) UNIQUE, mtime integer);
+CREATE TABLE content(id INTEGER PRIMARY KEY, fileid INTEGER REFERENCES file (id) ON DELETE CASCADE, page integer, content text);
 
--- Triggers to keep the FTS index up to date.
-CREATE TRIGGER file_ai AFTER INSERT ON file BEGIN
-  INSERT INTO file_fts(rowid, content) VALUES (new.id, new.content);
+CREATE VIRTUAL TABLE content_fts USING fts5(content, content='content', content_rowid='id');
+
+CREATE TRIGGER contents_ai AFTER INSERT ON content BEGIN
+  INSERT INTO content_fts(rowid, content) VALUES (new.id, new.content);
 END;
-CREATE TRIGGER file_ad AFTER DELETE ON file BEGIN
-  INSERT INTO file_fts(file_fts, rowid, content) VALUES('delete', old.id, old.content);
+CREATE TRIGGER contents_ad AFTER DELETE ON content BEGIN
+  INSERT INTO content_fts(content_fts, rowid, content) VALUES('delete', old.id, old.content);
 END;
-CREATE TRIGGER file_au AFTER UPDATE ON file BEGIN
-  INSERT INTO file_fts(file_fts, rowid, content) VALUES('delete', old.id, old.content);
-  INSERT INTO file_fts(rowid, content) VALUES (new.id, new.content);
+CREATE TRIGGER contents_au AFTER UPDATE ON content BEGIN
+  INSERT INTO content_fts(content_fts, rowid, content) VALUES('delete', old.id, old.content);
+  INSERT INTO content_fts(rowid, content) VALUES (new.id, new.content);
 END;
