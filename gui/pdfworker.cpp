78fb07573926955a8b3b35bb33bdaa4e9e52b55c
Albert S <mail@quitesimple.org>
2019-04-29 22:44:39 +0200

    Add destructor for pdfworker renderer

--- gui/pdfworker.cpp
+++ gui/pdfworker.cpp
@@ -21,6 +21,12 @@ struct Renderer
 		this->scaleX = scaleX;
 		this->scaleY = scaleY;
 	}
+
+	~Renderer()
+	{
+		qDeleteAll(documentcache);
+	}
+
 	Poppler::Document *document(QString path)
 	{
 		if(documentcache.contains(path))
@@ -29,6 +35,7 @@ struct Renderer
 		Poppler::Document *result = Poppler::Document::load(path);
 		if(result == nullptr)
 		{
+			// TODO: some kind of user feedback would be nicec
 			return nullptr;
 		}
 		result->setRenderHint(Poppler::Document::TextAntialiasing);
