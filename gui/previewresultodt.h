20d42a66a6a68a9dc604d5d9eaf22fb08b0ba388
Albert S <mail@quitesimple.org>
2022-07-24 11:41:25 +0200

    gui: Begin PreviewResultOdt

---
+++ gui/previewresultodt.h
@@ -0,0 +1,10 @@
+#ifndef PREVIEWRESULTODT_H
+#define PREVIEWRESULTODT_H
+#include "previewresultplaintext.h"
+
+class PreviewResultOdt : public PreviewResultPlainText
+{
+	using PreviewResultPlainText::PreviewResultPlainText;
+};
+
+#endif // PREVIEWRESULTODT_H
