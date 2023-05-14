c6c0b09e5607fc3a0f99ae0d4bdbc590e4c45b29
Albert S <mail@quitesimple.org>
2023-05-14 14:15:50 +0200

    PdfProcessor: Extract outline from documents

--- shared/pdfprocessor.h
+++ shared/pdfprocessor.h
@@ -1,5 +1,6 @@
 #ifndef PDFPROCESSOR_H
 #define PDFPROCESSOR_H
+#include <poppler-qt5.h>
 #include "processor.h"
 class PdfProcessor : public Processor
 {
@@ -7,7 +8,8 @@ class PdfProcessor : public Processor
 	PdfProcessor();
 
   public:
-	QVector<PageData> process(const QByteArray &data) const override;
+	QVector<DocumentOutlineEntry> createOutline(const QVector<Poppler::OutlineItem> &outlineItems) const;
+	DocumentProcessResult process(const QByteArray &data) const override;
 };
 
 #endif // PDFPROCESSOR_H
